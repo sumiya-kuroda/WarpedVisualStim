331b4264718c485840407a6d122b74903dbbd3f6
Sumiya Kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2025-07-31 15:26:56 +0100

    Update `daqlogger.py` to record nothing

--- WarpedVisualStim/daqlogger.py
+++ WarpedVisualStim/daqlogger.py
@@ -10,15 +10,18 @@ input('Press return to launch daqlogger')
 print('Launching daqlogger...')
 task_protocol = load_dumped_taskinfo()
 if task_protocol["ds_use_daqlogger"]:
+    record_ai = len(task_protocol["ds_daqlogger_ai_channels"]) > 0
+    record_ci = len(task_protocol["ds_daqlogger_ci_channels"]) > 0
+
     daqlogger = DAQLogger(dev_name=task_protocol["ds_sync_pulse_NI_dev"],
-                            ai_channels=task_protocol["ds_daqlogger_ai_channels"],
-                            ci_channels = task_protocol["ds_daqlogger_ci_channels"][0],
+                            ai_channels=task_protocol["ds_daqlogger_ai_channels"] if record_ai else [],
+                            ci_channels = task_protocol["ds_daqlogger_ci_channels"][0] if record_ci else '',
                             sample_rate = task_protocol["ds_daqlogger_sample_rate"],
                             sample_size = task_protocol["ds_daqlogger_sample_size"], 
                             osc_ip = task_protocol["ds_daqlogger_osc_ip"],
                             osc_port = task_protocol["ds_daqlogger_osc_port"],
-                            osc_address_ai = task_protocol["ds_daqlogger_osc_address_ai"],
-                            osc_address_ci = task_protocol["ds_daqlogger_osc_address_ci"],
+                            osc_address_ai = task_protocol["ds_daqlogger_osc_address_ai"] if record_ai else '',
+                            osc_address_ci = task_protocol["ds_daqlogger_osc_address_ci"] if record_ci else '',
                             save_file_location_ai = os.path.join(expanduser("~"), '.daqlogger_temp_ai.bin'),
                             save_file_location_ci = os.path.join(expanduser("~"), '.daqlogger_temp_ci.bin'))
 else:
